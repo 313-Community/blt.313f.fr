bd2302899069267b56470c35e58632100ca52178
Henri Golo <henri.golo.313f@gmail.com>
2025-08-03 23:39:09 +0200

    optimized token signing

--- src/lib/auth/wargaming.ts
+++ src/lib/auth/wargaming.ts
@@ -1,8 +1,7 @@
-import { ORIGIN, CLIENT_ID, SECRET } from '$env/static/private';
+import { CLIENT_ID, ORIGIN, SECRET } from '$env/static/private';
 import { handlers, type Provider, providers } from '$lib/auth';
 import { redirect } from '@sveltejs/kit';
 import jwt from 'jsonwebtoken';
-import * as crypto from 'node:crypto';
 
 export type WGToken = {
 	access_token: string;
@@ -12,25 +11,14 @@ export type WGToken = {
 	timer?: ReturnType<typeof setTimeout>;
 };
 
-export function hashData(data: object): string {
-	return crypto.createHmac('sha256', SECRET).update(JSON.stringify(data)).digest('hex');
-}
-
-export function verifHash(data: object, hash: string): boolean {
-	if (!hash) return false;
-	return hashData(data) === hash;
-}
-
 export function genToken(payload: Record<string, string>): string {
-	return jwt.sign({ payload, hash: hashData(payload) }, SECRET, { expiresIn: '1d' });
+	return jwt.sign(payload, SECRET, { expiresIn: '1d' });
 }
 
 // Vérifie que les informations d'un JWT sont correctes
 export function verifToken(token: string): Record<string, string> | null {
 	try {
-		const { payload, hash } = jwt.verify(token, SECRET) as { payload: Record<string, string>; hash: string };
-		if (!verifHash(payload, hash)) return null;
-		return payload;
+		return jwt.verify(token, SECRET) as Record<string, string>;
 	} catch {
 		return null;
 	}
@@ -90,7 +78,6 @@ if (!providers.some((provider: Provider) => provider.id === wg.id)) {
 	handlers.push(async ({ event, resolve }) => {
 		if (event.url.pathname === '/api' + callback) {
 			const realm = event.url.searchParams.get('realm') || 'eu';
-			console.log('realm', realm);
 			throw redirect(302, issuer.toString().replace('_realm_', realm));
 		} else if (event.url.pathname === callback) {
 			const { realm } = verifToken(event.cookies.get(cookieName) || '') || { realm: 'eu' };
