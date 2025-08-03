03302c88d72235d3674d0128357a9cf6343ae68a
Henri Golo <henri.golo.313f@gmail.com>
2025-08-03 23:19:51 +0200

    fixed wrong domain name for NA

--- src/routes/(regular)/+page.server.ts
+++ src/routes/(regular)/+page.server.ts
@@ -1,25 +1,26 @@
 import { CLIENT_ID } from '$env/static/private';
 import type { PageServerLoad } from './$types';
 import { redirect, type ServerLoadEvent } from '@sveltejs/kit';
+import { cookieName, verifToken } from '$lib/auth/wargaming';
 
 export const load: PageServerLoad = async ({ cookies, fetch }: ServerLoadEvent) => {
-	const _realm = cookies.get('realm') || 'eu';
-	const realm = _realm.toLowerCase() === 'na' ? 'na' : _realm;
-	const access_token = cookies.get('access_token');
-	const account_id = cookies.get('account_id');
-	if (!access_token || !account_id) {
-		throw redirect(302, '/login');
-	}
-	const url = new URL(`https://api.worldofwarships.${realm}/wows/account/info/`);
-	url.searchParams.set('application_id', CLIENT_ID);
-	url.searchParams.set('account_id', account_id);
-	url.searchParams.set('access_token', access_token);
-	url.searchParams.set('fields', 'private.battle_life_time,created_at');
-	const res = await fetch(url);
-	try {
-		const { data } = await res.json();
-		return data[account_id];
-	} catch {
-		return { private: { battle_life_time: 0 }, created_at: Date.now() / 1000 };
-	}
+  const { realm: _realm } = verifToken(cookies.get(cookieName) || '') || { realm: 'eu' };
+  const realm = _realm.toLowerCase() === 'na' ? 'na' : _realm;
+  const access_token = cookies.get('access_token');
+  const account_id = cookies.get('account_id');
+  if ( !access_token || !account_id ) {
+    throw redirect(302, '/login');
+  }
+  const url = new URL(`https://api.worldofwarships.${realm}/wows/account/info/`);
+  url.searchParams.set('application_id', CLIENT_ID);
+  url.searchParams.set('account_id', account_id);
+  url.searchParams.set('access_token', access_token);
+  url.searchParams.set('fields', 'private.battle_life_time,created_at');
+  const res = await fetch(url);
+  try {
+    const { data } = await res.json();
+    return data[account_id];
+  } catch {
+    return { private: { battle_life_time: 0 }, created_at: Date.now() / 1000 };
+  }
 };
